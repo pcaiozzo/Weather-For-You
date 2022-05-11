0169153a03bddb4864d7096c13b88149f2a43cf7
pcaiozzo <paigecaiozzo@gmail.com>
2022-05-11 10:55:55 -0400

    updated layout for website

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const form = document.querySelector(".top-banner form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const inputVal = input.value;
+});
+
+const apiKey = "09a768f72164141fe50ccdf4967f1388";
+const inputVal = input.value;
